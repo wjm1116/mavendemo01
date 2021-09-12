f4fce38744ac82d51f69994a323af3089c08865e
“wjm1116” <573919325@qq.com>
2021-09-12 23:03:46 +0800

    Initial commit

---
+++ src/test/java/com/lemon/exercises/Student.java
@@ -0,0 +1,31 @@
+package com.lemon.exercises;
+
+public class Student {
+    private String name;
+    private int age;
+    private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
